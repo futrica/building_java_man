453af5f63ace393432a4cf8e4bc5790d686bc753
futrica <jmfutrica@gmail.com>
2013-07-08 22:31:15 -0300

    java.io

---
+++ caelum/javaio/src/br/futrica/testeio/TesteIo.java
@@ -0,0 +1,22 @@
+package br.futrica.testeio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class TesteIo {
+	public static void main(String[] args) throws IOException {
+		InputStream is = System.in;
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		
+		String linha = br.readLine();
+		
+		while(linha != null){
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+	}
+
+}
