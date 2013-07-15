75f117303aada8c9fe2a7456cc3aa62c8046701f
futrica <jmfutrica@gmail.com>
2013-07-14 22:40:47 -0300

    + java.io

--- caelum/javaio/src/br/futrica/testeio/TesteIo.java
+++ caelum/javaio/src/br/futrica/testeio/TesteIo.java
@@ -1,22 +1,31 @@
 package br.futrica.testeio;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+
 
 public class TesteIo {
 	public static void main(String[] args) throws IOException {
 		InputStream is = System.in;
+		//InputStream is = new FileInputStream("arquivo.txt");
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
+		
+		OutputStream os = new FileOutputStream("saida.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
+
 		
 		String linha = br.readLine();
 		
 		while(linha != null){
 			System.out.println(linha);
+			bw.write(linha); // copiando para outro arquivo 
 			linha = br.readLine();
+			bw.newLine();
 		}
+		
+		br.close();
+		bw.close();
 	}
 
 }
