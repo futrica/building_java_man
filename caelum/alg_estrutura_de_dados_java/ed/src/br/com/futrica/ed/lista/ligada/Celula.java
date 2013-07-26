7d93f28c37e5298505906d88073c5508ab83ce56
futrica <jmfutrica@gmail.com>
2013-07-25 22:01:10 -0300

    linked list

---
+++ caelum/alg_estrutura_de_dados_java/ed/src/br/com/futrica/ed/lista/ligada/Celula.java
@@ -0,0 +1,37 @@
+package br.com.futrica.ed.lista.ligada;
+
+public class Celula {
+	private Celula proxima;
+	private Celula anterior;
+	private Object elemento;
+	
+	public Celula(Celula proxima, Object elemento) {
+		this.proxima = proxima;
+		this.elemento = elemento;
+	}
+
+	public Celula(Object elemento) {
+		this.elemento = elemento;
+	}
+	
+	public void setProxima(Celula proxima) {
+		this.proxima = proxima;
+	}
+
+	public Celula getProxima() {
+		return proxima;
+	}
+
+	public void setAnterior(Celula anterior) {
+		this.anterior = anterior;
+	}
+
+	public Celula getAnterior() {
+		return anterior;
+	}
+	
+	public Object getElemento() {
+		return elemento;
+	}
+	
+}
